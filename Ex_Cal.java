90df687fa93ddd37f675f6fe684f53c89ee8efa0
Ramya B <01fe22bca202@kletech.ac.in>
2024-02-20 11:00:06 +0530

    adding two files for extended Calculator Branch

---
+++ Ex_Cal.java
@@ -0,0 +1,20 @@
+class Ex_Cal{
+    /**
+         * The function "square" takes a number as input and returns the square of that number.
+         * 
+         * @param num3 The parameter "num3" is a double data type, which means it can hold decimal values.
+         * @return The square of the input number.
+         */
+        public static double square(double num3){
+            return num3 * num3;
+        }
+        /**
+         * The function cube takes a number as input and returns the cube of that number.
+         * 
+         * @param num4 The parameter "num4" is a double data type, which means it can hold decimal values.
+         * @return The cube of the input number.
+         */
+        public static double cube(double num4){
+            return num4 * num4 * num4;
+        }
+    }
